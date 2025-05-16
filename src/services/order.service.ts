404911128a358ab328fee9e47dfe15cb9dbbbd09
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-05-16 14:09:30 +0600

    order service

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,1 +1,33 @@
- 
+
+import * as orderModel from '../models/order.model';
+import { OrderStatus } from '@prisma/client';
+
+export const createOrder = async (orderData: {
+  userId: string;
+  productIds: string[];
+  total: number;
+  status?: OrderStatus;
+}) => {
+  return await orderModel.createOrder(orderData);
+};
+
+export const getAllOrders = async () => {
+  return await orderModel.getAllOrders();
+};
+
+export const getOrderById = async (id: string) => {
+  return await orderModel.getOrderById(id);
+};
+
+export const updateOrderStatus = async (id: string, status: OrderStatus) => {
+  return await orderModel.updateOrderStatus(id, status);
+};
+
+export const deleteOrder = async (id: string) => {
+  return await orderModel.deleteOrder(id);
+};
+
+export const getUserOrders = async (userId: string) => {
+  return await orderModel.getOrdersByUserId(userId);
+};
+
