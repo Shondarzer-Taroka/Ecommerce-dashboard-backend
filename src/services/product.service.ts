cbcaf3043f9eb52ca8aa7a901a6a1dfe19c98c55
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-05-16 14:08:51 +0600

    product service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,1 +1,23 @@
  
+import * as productModel from '../models/product.model';
+import { Product } from '@prisma/client';
+
+export const createProduct = async (productData: Omit<Product, 'id' | 'createdAt' | 'updatedAt'>) => {
+  return await productModel.createProduct(productData);
+};
+
+export const getAllProducts = async () => {
+  return await productModel.getAllProducts();
+};
+
+export const getProductById = async (id: string) => {
+  return await productModel.getProductById(id);
+};
+
+export const updateProduct = async (id: string, productData: Partial<Product>) => {
+  return await productModel.updateProduct(id, productData);
+};
+
+export const deleteProduct = async (id: string) => {
+  return await productModel.deleteProduct(id);
+};
